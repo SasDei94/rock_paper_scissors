a47fcedcfed00193e1dda258769e394336f1dfba
SasDei94 <sascha.deischitz@gmx.at>
2022-01-27 12:12:29 +0100

    update

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors');
-
 let playerScore = 0;
 let compScore = 0;
+const buttons = document.querySelectorAll("input")
 
 function computerPlay() {
 
-    let compChoice = (Math.random());   
+    let compChoice = (Math.random())
 
     if (compChoice < 0.34) {
         return "rock"
@@ -20,58 +17,53 @@ function computerPlay() {
     }
 }
 
-function userChoice() {
-
-	var userInput = prompt("Please enter your choice of either rock, paper or scissors. To exit, hit the 'Cancel' button.");
-	return userInput.toLowerCase();
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-function rockpaperscissors(playerChoice, compChoice) {
+function rockpaperscissors(playerChoice) {
 
-    var compChoice = computerPlay();
+    let compChoice = computerPlay();
 
-    var playerChoice = userChoice();
+    let result = ""
 
-    if (playerScore == 5) {
-        alert("You already won! Press F5 for another game.");
+
+    if ((playerChoice == "rock" && compChoice == "scissors") ||
+        (playerChoice == "scissors" && compChoice == "paper") ||
+        (playerChoice == "paper" && compChoice == "rock")) {
+        
+        playerScore += 1
+        result = ("You win! " + playerChoice + " beats " + compChoice
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + compScore)
+
+        if (playerScore == 5) {
+            result += "<br><br>You won the game! <br>Press F5 or click on the \"New Game\" button to play again."
+            disableButtons()
+        }
+    }
+    else if (playerChoice == compChoice) {
+        result = ("It\"s a tie. You both chose " + playerChoice
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + compScore)
+    }
+    else {
+        compScore += 1
+        result = ("You lose! " + compChoice + " beats " + playerChoice
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + compScore)
+
+        if (compScore == 5) {
+            result += "<br><br>The computer won the game! <br>Press F5 or click on the \"New Game\" button to play again."
+            disableButtons()
+        }
     }
 
-    else if (compScore == 5) {
-        alert("You already lost! Press F5 for another game.")
-    }
-
-
-    if (playerChoice == compChoice) {
-        alert("Tie!");
-    
-
-    } else if (playerChoice == "rock" && compChoice == "scissors") {
-        playerScore ++;
-        alert("You won this round!");
-    
-    
-    } else if (playerChoice == "scissors" && compChoice == "rock") {
-        compScore ++;
-        alert("You lost this round!");
-    
-     
-    } else if (playerChoice == "paper" && compChoice == "rock") {
-        playerScore ++;
-        alert("You won this round!");
-    
-    
-    } else if (playerChoice == "rock" && compChoice == "paper") {
-        compScore ++;
-        alert("You lost this round!");
-
-
-    } else if (playerChoice == "scissors" && compChoice == "paper") {
-        playerScore ++;
-        alert("You won this round!");
-
-
-    } else if (playerChoice == "paper" && compChoice == "scissors") {
-        compScore ++;
-        alert("You lost this round!");
-    }
+    document.getElementById("result").innerHTML = result
+    return
 }
+
+buttons.forEach(button =>{
+    button.addEventListener("click", function(){
+        rockpaperscissors(button.value)
+    })
+})
